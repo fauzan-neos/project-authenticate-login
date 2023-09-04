6213e7eeddb1db8a792c884d1597c88854bdf362
fauzan-neos <afauzan0311@gmail.com>
2023-09-04 19:51:16 +0700

    Update route

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,7 @@ Route::get('/', function() {
     ]);
 });
 
-Route::get('/login', [LoginController::class, 'index'])->name('login')->middleware('guest');
+Route::get('/login', [LoginController::class, 'index'])->middleware('guest');
 Route::post('/login', [LoginController::class, 'authenticate']);
 Route::post('/logout', [LoginController::class, 'logout']);
 
